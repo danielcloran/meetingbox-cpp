484be3f03d483ae4b36232eb72f6e4992de1fe76
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-03-22 14:54:37 -0400

    install.sh updates

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 if [ -d "./vcpkg" ]; then
     echo "Install script already run."
 else
+    sudo apt install -y ninja-build
     git clone https://github.com/Microsoft/vcpkg.git
     ./vcpkg/bootstrap-vcpkg.sh
 
-    # If on RaspberryPi, install the RGB LED Matrix library
-
-    if grep -q "BCM" /proc/cpuinfo; then
+    # If on RaspberryPi, and folder doesn't exist install the RGB LED Matrix library
+    if grep -q "BCM" /proc/cpuinfo && [ ! -d "./libs/rpi-rgb-led-matrix" ]; then
         echo "This is a Raspberry Pi."
         git clone https://github.com/hzeller/rpi-rgb-led-matrix ./libs/rpi-rgb-led-matrix
         make -C libs/rpi-rgb-led-matrix/
