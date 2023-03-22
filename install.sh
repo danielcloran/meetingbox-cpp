e35bbd446e2502b2978d1625bbabc81675ccca4d
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-03-22 15:06:27 -0400

    install.sh changes

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 if [ -d "./vcpkg" ]; then
     echo "Install script already run."
 else
-    sudo apt install -y ninja-build
-    git clone https://github.com/Microsoft/vcpkg.git
-    ./vcpkg/bootstrap-vcpkg.sh
-
     # If on RaspberryPi, and folder doesn't exist install the RGB LED Matrix library
     if grep -q "BCM" /proc/cpuinfo && [ ! -d "./libs/rpi-rgb-led-matrix" ]; then
         echo "This is a Raspberry Pi."
+        sudo apt install -y ninja-build
         git clone https://github.com/hzeller/rpi-rgb-led-matrix ./libs/rpi-rgb-led-matrix
         make -C libs/rpi-rgb-led-matrix/
     fi
 
+    git clone https://github.com/Microsoft/vcpkg.git ./libs/vcpkg
+    ./libs/vcpkg/bootstrap-vcpkg.sh
+
 fi
 
 # Install dependencies
-./vcpkg/vcpkg install "aws-sdk-cpp[lex]" --recurse
-./vcpkg/vcpkg install jsoncpp
-./vcpkg/vcpkg install sfml
-./vcpkg/vcpkg install eventpp
+./libs/vcpkg/vcpkg install "aws-sdk-cpp[lex]" --recurse
+./libs/vcpkg/vcpkg install jsoncpp
+./libs/vcpkg/vcpkg install sfml
+./libs/vcpkg/vcpkg install eventpp
 
-./vcpkg/vcpkg integrate install
+./libs/vcpkg/vcpkg integrate install
