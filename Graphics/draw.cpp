fd23d04c32fe35938c1447fa156715df2bec4606
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-03-23 17:09:51 -0400

    dif

--- Graphics/draw.cpp
+++ Graphics/draw.cpp
@@ -22,7 +22,7 @@ void draw()
     defaults.chain_length = 2;
     defaults.parallel = 3;
     defaults.show_refresh_rate = true;
-    defaults.pwm_lsb_nanoseconds = 9;
+    defaults.pwm_lsb_nanoseconds = 50;
 
     runtime_opt.gpio_slowdown = 4;
     Canvas *canvas = CreateMatrixFromOptions(defaults, runtime_opt);
