1f98855f6a0ad66232d9d2f40dcf8961bed18741
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-03-23 17:14:47 -0400

    remove dither

--- Graphics/draw.cpp
+++ Graphics/draw.cpp
@@ -23,7 +23,7 @@ void draw()
     defaults.parallel = 3;
     defaults.show_refresh_rate = true;
     defaults.pwm_lsb_nanoseconds = 50;
-    defaults.pwm_dither_bits = 2;
+
 
     runtime_opt.gpio_slowdown = 4;
     Canvas *canvas = CreateMatrixFromOptions(defaults, runtime_opt);
