da60f3e82e36289021e48ad6b39afc3d4e2f469f
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-03-23 17:50:38 -0400

    pulse

--- Graphics/draw.cpp
+++ Graphics/draw.cpp
@@ -24,7 +24,7 @@ void draw()
     defaults.show_refresh_rate = true;
     defaults.pwm_lsb_nanoseconds = 50;
     // turn off hardware pulse
-    defaults.no_hardware_pulse = true;
+    defaults.hardware_pulse = false;
 
     runtime_opt.gpio_slowdown = 4;
     Canvas *canvas = CreateMatrixFromOptions(defaults, runtime_opt);
