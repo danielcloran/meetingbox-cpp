f5cb066cd7ed9db7a08a94564ae9ad78cc022e7e
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-03-23 16:57:14 -0400

    minmal

--- Graphics/draw.cpp
+++ Graphics/draw.cpp
@@ -18,7 +18,7 @@ void draw()
     defaults.chain_length = 1;
     defaults.parallel = 1;
     defaults.show_refresh_rate = true;
-    Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
+    Canvas *canvas = RGBMatrix::CreateFromFlags([], [], &defaults);
     if (canvas == NULL)
         return;
 
