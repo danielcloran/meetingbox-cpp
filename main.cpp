7c411dcd7ff95a470033112528e4ab5bcfa14458
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-03-28 19:02:03 -0400

    delay

--- main.cpp
+++ main.cpp
@@ -36,13 +36,13 @@ int main(int argc, char *argv[])
 
     while (!quit)
     {
-        // while (SDL_PollEvent(&event))
-        // {
-        //     if (event.type == SDL_QUIT)
-        //     {
-        //         quit = true;
-        //     }
-        // }
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
 
         SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 100, 0));
 
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
             angle = 0.0f;
         }
 
-        // SDL_Delay(10);
+        SDL_Delay(20);
     }
 
     SDL_FreeSurface(surface);
