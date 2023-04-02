2ff843240fd2de0d6c7b0aec88237130bbae1016
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-04-02 18:06:48 -0400

    audio

--- graphics/src/graphics.cpp
+++ graphics/src/graphics.cpp
@@ -3,7 +3,7 @@
 
 void run()
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING & ~SDL_INIT_VIDEO) < 0)
+    if (SDL_Init(SDL_INIT_EVERYTHING & ~SDL_INIT_VIDEO & ~SDL_INIT_AUDIO) < 0)
     {
         std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
         return;
