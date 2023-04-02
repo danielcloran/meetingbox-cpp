e1c5fcb27895b84ba073b658b346bc76b8796135
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-04-02 19:47:20 -0400

    RGB

--- graphics/src/rpi/draw.cpp
+++ graphics/src/rpi/draw.cpp
@@ -27,7 +27,7 @@ void init()
     defaults.parallel = 3;
     defaults.show_refresh_rate = true;
     defaults.pwm_lsb_nanoseconds = 50;
-    defaults.led_rgb_sequence = "GBR";
+    defaults.led_rgb_sequence = "RBG";
 
     // turn off hardware pulse
     // defaults.disable_hardware_pulsing = true;
