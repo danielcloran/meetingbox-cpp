9b5175360fb1d118c8b971b27888666c4ab17abf
Daniel Cloran <29779316+danielcloran@users.noreply.github.com>
2023-04-13 17:02:05 -0400

    test

--- graphics/src/rpi/renderer.cpp
+++ graphics/src/rpi/renderer.cpp
@@ -77,7 +77,7 @@ void Renderer::draw(SDL_Surface *surface)
     std::cout << "Post copy" << std::endl;
 
     // mask the different pixels to pixelMask
-    std::transform(pixelData[currentBuffer].begin(), pixelData[currentBuffer].end(), priorPixelData.begin(), maskPixelData.begin(), [](uint32_t a, uint32_t b) { return a ^ b; });
+    std::transform(pixelData[currentBuffer].begin(), pixelData[currentBuffer].end(), pixelData[!currentBuffer].begin(), maskPixelData.begin(), [](uint32_t a, uint32_t b) { return a ^ b; });
 
     // off_screen_canvas_->Clear();
 
